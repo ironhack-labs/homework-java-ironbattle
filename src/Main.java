90cd5e88ce5623dbbec6b11894f9217fb8e41a40
jtiradohernandez <javiertirhe@gmail.com>
2024-02-03 13:54:55 +0100

    Feat: Creation of the logic of the battle

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,43 @@
 public class Main {
     public static void main(String[] args){
+        //Temporary creation of characters, can be deleted
         Character char1 = new Character("guerrero",12);
+        Character char2 = new Character("wizard",12);
 
-        System.out.println(char1.getHp());
-        System.out.println(char1.getName());
+        //Call for the method that will do the combat
+        combat(char1,char2);
+    }
 
+
+    public static void combat(Character char1, Character char2){
+        boolean finished = false;
+        int initialHpChar1 = char1.getHp();
+        int initialHpChar2 = char2.getHp();
+
+        //Loop until one of the character wins
+        while(finished == false){
+
+            //Loop for the battle
+            while(char1.getHp()>0||char2.getHp()>0){
+                char1.attack(char2);
+                char2.attack(char1);
+            }
+
+            //Once the battle is finished, let's check who wins the battle
+            if (0>=char1.getHp()){
+                //Character 2 wins the battle
+                System.out.println("The player "+char2.getName()+" wins the battle!");
+                finished = true;
+            }else if(0>=char2.getHp()){
+                //Character 1 wins the battle
+                System.out.println("The player "+char1.getName()+" wins the battle!");
+                finished = true;
+            }else{
+                //The battle result is a tie
+                System.out.println("The battle results in a tie! The battle will be repeated");
+                char1.setHp(initialHpChar1);
+                char2.setHp(initialHpChar2);
+            }
+        }
     }
 }
