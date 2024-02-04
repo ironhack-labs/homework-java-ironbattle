c41149d221d2ee4023ccb5317cb3332b71f8a2f7
Katia <katia.obr@gmail.com>
2024-02-04 18:35:54 +0100

    add Attacker and Character, first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args){
+        Warrior war1 = new Warrior("Aragorn", 100);
+        Warrior war2 = new Warrior("Boromir", 50);
+        System.out.println(war2.getId());
+    }
+}
