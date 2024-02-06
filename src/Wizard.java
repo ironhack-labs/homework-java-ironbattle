6809c112e1fcd565221097c9661c612c3364ca1f
ivandavid858 <ivandavid858@gmail.com>
2024-02-06 19:17:41 +0100

    Create Wizard class

---
+++ src/Wizard.java
@@ -0,0 +1,36 @@
+public class Wizard extends Character implements  Attacker {
+
+    private int stamina;
+    private int strength;
+
+    public Wizard(String name, int hp, int stamina, int strength) {
+        super(name, hp);
+        setStamina(stamina);
+        setStrength(strength);
+    }
+
+
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public void attack(Character character) {
+
+    }
+
+
+}
