71b79030c33f08f3c06b524f9d6cb92c730c49fc
celularfreak <celularfreak@hotmail.com>
2024-02-06 21:48:27 +0100

    Wizard first blueprint

--- src/Wizard.java
+++ src/Wizard.java
@@ -1,2 +1,24 @@
-public class Wizard {
+public class Wizard extends Character{
+
+    private int mana;
+    private int intelligence;
+
+    public Wizard(String name) {
+        super(name, 0);
+        setMana();
+        setIntelligence();
+    }
+
+    public void setMana(){
+        this.mana = mana;
+    }
+
+    public void setIntelligence(){
+        this.intelligence = intelligence;
+    }
+
+    public void attack(){
+        System.out.println("atacking");
+    }
+
 }
