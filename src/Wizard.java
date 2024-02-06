42fe239ff129cb7b96834843b0534df377e8e8e5
ivandavid858 <ivandavid858@gmail.com>
2024-02-06 21:31:56 +0100

    Add mana and intelligence attributes to the Wizard

--- src/Wizard.java
+++ src/Wizard.java
@@ -2,14 +2,22 @@ public class Wizard extends Character implements Attacker {
 
     private final int MIN_HP_WIZARD = 50;
     private final int MAX_HP_WIZARD = 100;
+    private final int MIN_MANA_WIZARD = 10;
+    private final int MAX_MANA_WIZARD = 50;
+    private final int MIN_INTELLIGENCE_WIZARD = 1;
+    private final int MAX_INTELLIGENCE_WIZARD = 50;
 
     private int stamina;
     private int strength;
+    private int mana;
+    private int intelligence;
 
-    public Wizard(String name, int hp, int stamina, int strength) {
+    public Wizard(String name, int hp, int stamina, int strength, int mana) {
         super(name, hp);
         setStamina(stamina);
         setStrength(strength);
+        setMana((Math.random() * (MAX_MANA_WIZARD - MIN_MANA_WIZARD + 1)) + MIN_MANA_WIZARD);
+        setIntelligence((Math.random() * (MAX_INTELLIGENCE_WIZARD - MIN_INTELLIGENCE_WIZARD + 1)) + MIN_INTELLIGENCE_WIZARD);
     }
 
     public int getStamina() {
@@ -28,9 +36,39 @@ public class Wizard extends Character implements Attacker {
         this.strength = strength;
     }
 
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        validateMana(mana);
+        this.mana = mana;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        validateIntelligence(intelligence);
+        this.intelligence = intelligence;
+    }
+
     @Override
     public void attack(Character character) {
 
+    }
+
+    public void validateMana(int mana) {
+        if (mana < MIN_MANA_WIZARD && mana > MAX_MANA_WIZARD) {
+            throw new IllegalArgumentException("El mana debe estar entre " + MIN_MANA_WIZARD + " y " + MAX_MANA_WIZARD);
+        }
+    }
+
+    public void validateIntelligence(int intelligence) {
+        if (intelligence < MIN_INTELLIGENCE_WIZARD && intelligence > MAX_INTELLIGENCE_WIZARD) {
+            throw new IllegalArgumentException("La inteligencia del Wizard debe estar entre " + MIN_INTELLIGENCE_WIZARD + " y " + MAX_INTELLIGENCE_WIZARD);
+        }
     }
 
     public void validateHp(int hp) {
