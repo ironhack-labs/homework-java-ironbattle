cb5911349cdadb58e44586ace55c3de6726de600
Darren <111098562+DJH23@users.noreply.github.com>
2024-02-07 12:40:18 +0100

    temp commit to enable branch change

--- src/Utils.java
+++ src/Utils.java
@@ -1,6 +1,7 @@
 import java.util.concurrent.ThreadLocalRandom;
+
 public class Utils {
-     // Generates a random integer within a specified range.
+    // Generates a random integer within a specified range.
     public static int generateRandomInt(int min, int max) {
         if (min > max) {
             throw new IllegalArgumentException("Max must be greater than min");
@@ -8,4 +9,6 @@ public class Utils {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
     // Other utility methods to be added here
+
+
 }
