9fe52e8853fceb47b5254841325ef14332f830a1
Darren <111098562+DJH23@users.noreply.github.com>
2024-02-07 12:51:21 +0100

    update min max conditional so that min can't be equal to max: min >= max

--- src/Utils.java
+++ src/Utils.java
@@ -3,7 +3,7 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Utils {
     // Generates a random integer within a specified range.
     public static int generateRandomInt(int min, int max) {
-        if (min > max) {
+        if (min >= max) {
             throw new IllegalArgumentException("Max must be greater than min");
         }
         return ThreadLocalRandom.current().nextInt(min, max + 1);
