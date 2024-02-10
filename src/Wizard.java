2c9195e1049c999b018d76fb32a6218d9687f596
ivandavid858 <ivandavid858@gmail.com>
2024-02-10 13:38:46 +0100

    Implementing attack method for the Wizard character

--- src/Wizard.java
+++ src/Wizard.java
@@ -9,7 +9,8 @@ public class Wizard extends Character implements Attacker {
     private final int MANA_TO_DECREASE_WHEN_FIREBALL = 5;
     private final int DAMAGE_INFLICTED_BY_STAFF_HIT = 2;
     private final int MANA_TO_DECREASE_WHEN_STAFF_HIT = 1;
-
+    private final int CONST_FOR_FIREBALL = 0;
+    private final int CONST_FOR_STAFF_HIT = 1;
     private int mana;
     private int intelligence;
 
@@ -39,10 +40,9 @@ public class Wizard extends Character implements Attacker {
 
     @Override
     public void attack(Character character) {
-        int randomCast = 1;
-        if (randomCast == 1) {
+        if (this.getMana() >= MANA_TO_DECREASE_WHEN_FIREBALL && Utils.generateRandomInt(CONST_FOR_FIREBALL, CONST_FOR_STAFF_HIT) == CONST_FOR_FIREBALL) {
             castFireball(character);
-        }  else {
+        } else {
             castStaffHit(character);
         }
     }
@@ -54,7 +54,7 @@ public class Wizard extends Character implements Attacker {
 
     private void castStaffHit(Character character) {
         character.setHp(character.getHp() - DAMAGE_INFLICTED_BY_STAFF_HIT);
-        this.setMana(this.getMana() - MANA_TO_DECREASE_WHEN_STAFF_HIT);
+        this.setMana(this.getMana() + MANA_TO_DECREASE_WHEN_STAFF_HIT);
     }
 
     public void validateMana(int mana) {
