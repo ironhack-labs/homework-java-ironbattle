52c196e0335716f322ff330f7734214c21e9917e
esmartdie <esmartdie@gmail.com>
2024-02-17 22:26:50 +0100

    solving recursion problems and add custom printing

--- src/Main.java
+++ src/Main.java
@@ -36,38 +36,18 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args){
-
 
         StartGame start = new StartGame();
         List<Character> players = start.setUp();
         Character Player1 = players.get(0);
         Character Player2 = players.get(1);
 
-
-
-
-            //Character Player1 = new Warrior("Guerrerito");
-            //Character Player2 = new Wizard("Maguito");
-
         Battle battle = new Battle(Player1, Player2);
         battle.startBattle(Player1, Player2);
 
-        for (String s: battle.getBattleRecord()){
-            System.out.println(s);
-            waitingTime();
-        }
+        battle.battlePrinter(battle.getBattleRecord(), 1);
 
-    }
-
-    private static void waitingTime() {
-        try {
-            Thread.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-      
     }
 
 }
