0751917939e85b8b13041bb84d2eeb86bb533d65
jtiradohernandez <javiertirhe@gmail.com>
2024-02-17 12:10:23 +0100

    Refactor: move combat function inside Combat class

--- src/Main.java
+++ src/Main.java
@@ -50,7 +50,7 @@ public class Main {
                         break;
                     case 4:
                         //Start the battle
-                        combat(char1, char2);
+                        Combat.startCombat(char1, char2);
                         is_finish = true;
                         break;
                     default:
@@ -196,61 +196,4 @@ public class Main {
         }
         return character;
 }
-
-    public static void combat(Character char1, Character char2) {
-        boolean finished = false;
-        char1.setTeam(Team.A);
-        char2.setTeam(Team.B);
-
-        Bard.introducesStory();
-
-        Character initChar1 = char1.clone();
-
-        Bard.introducesFirstOpponent(char1);
-
-        Character initChar2 = char2.clone();
-
-        Bard.introducesSecondOpponent(char2);
-
-        int roundCounter = 1;
-        //Loop until one of the character wins
-        while (!finished) {
-
-            //Loop for the battle
-            while (char1.isAlive() && char2.isAlive()) {
-                Bard.announcesRound(roundCounter++);
-
-                char1.attack(char2);
-                Bard.narratesHp(char2);
-
-                char2.attack(char1);
-                Bard.narratesHp(char1);
-
-                if (char1.getHp() <= 0) {
-                    char1.setAlive(false);
-                    Bard.announcesOpponentDeath(char1);
-                }
-                if (char2.getHp() <= 0) {
-                    char2.setAlive(false);
-                    Bard.announcesOpponentDeath(char2);
-                }
-            }
-
-            //Once the battle is finished, let's check who wins the battle
-            if (!char2.isAlive() && !char1.isAlive()) {
-                //The battle result is a tie
-                Bard.announcesTie();
-                roundCounter = 0;
-                char1 = initChar1.clone();
-                char2 = initChar2.clone();
-
-            } else if (!char2.isAlive()) {
-                Bard.announcesOpponentVictory(char1);
-                finished = true;
-            } else if (!char1.isAlive()) {
-                Bard.announcesOpponentVictory(char2);
-                finished = true;
-            }
-        }
-    }
 }
