aa97f6ab49a3d62f2dd06165bcc7d8dab6835084
annadpp <anna.depablo.puig@gmail.com>
2024-02-19 19:42:51 +0100

    Add Wizard attacks

--- src/Wizard.java
+++ src/Wizard.java
@@ -34,6 +34,38 @@ public class Wizard extends Character implements Attacker {
     // Attack
     @Override
     public void attack(Character character) {
+        Random random = new Random();
+        String attackType;
 
+        attackType = random.nextBoolean() ? "fireball" : "staffHit";
+        int damage = 0;
+
+        switch (attackType) {
+            case "fireball":
+                if (this.mana >= 5) {
+                    damage = this.intelligence;
+                    this.mana -= 5;
+                } else if (this.mana <= 0) {
+                    this.mana = 0; // No mana for Fireball
+                } else {
+                    // No mana for a Fireball, do a Staff hit instead
+                    damage = 2;
+                    this.mana += 1;
+                }
+                break;
+
+            case "staffHit":
+                if (this.mana >= 1) {
+                    damage = 2;
+                    this.mana += 1;
+                } else {
+                    this.mana += 2; // Recover stamina by 2 if not enough stamina for Staff hit
+                }
+                break;
+
+            default:
+                // If the action is neither "Fireball" nor "Staff hit" -> just for convention
+                break;
+        }
     }
 }
