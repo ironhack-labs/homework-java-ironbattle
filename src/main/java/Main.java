6a81c524af617e45d156ebe53fd6ca9ca99d31b7
victoriaschwin <vicky.schwint95@gmail.com>
2024-02-20 22:41:22 +0100

    warrior bug fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class Main {
@@ -7,9 +8,9 @@ public class Main {
         int choiceMenu;
         Warrior newWarrior;
         Wizard newWizard;
+        List<Warrior> warriors = new ArrayList<>();
+        List<Wizard> wizards = new ArrayList<>();
 
-        
-        //Aqui comienza el programa batalla
         Scanner scanner = new Scanner(System.in);
 
         do {
@@ -19,14 +20,28 @@ public class Main {
             switch (choiceMenu) {
                 case 1:
                     newWarrior = createWarrior(scanner);
+                    warriors.add(newWarrior);
                     break;
                 case 2:
                     newWizard = createWizard(scanner);
+                    wizards.add(newWizard);
                     break;
                 case 3:
-                    //batalla
+                    if(warriors.size() == 1 && wizards.size() == 1){
+                        //Opuestos
+                        battleOpposite(warriors.get(0),wizards.get(0));
+                    }else if(warriors.size() == 2){
+                        //Caso ambos warriors
+                        battleWarriors(warriors.get(0), warriors.get(1));
+                    }else if(wizards.size() == 2) {
+                        //Caso ambos wizards
+                        battleWizards(wizards.get(0), wizards.get(1));
+                    }else{
+                        throw new IllegalArgumentException("Please create two characters to start a battle.");
+                    }
+
                 case 4:
-                    //batalla randomizada
+
                     Generator generator = new Generator();
 
                     //Random class selection
@@ -43,17 +58,20 @@ public class Main {
 
                     //Class declaration
                     if (character1.equals("Wizard")){
-                        Wizard ch1 = new Wizard( "Character1", generator.generatorWizard(), generator.generatorMana(), generator.generatorIntelligence() );
+                        wizards.add(new Wizard( "Character1", generator.generatorWizard(), generator.generatorMana(), generator.generatorIntelligence() ));
                     } else {
-                        Warrior ch1 = new Warrior( "Character2", generator.generatorWarrior(), generator.generatorMana(), generator.generatorStrength());
+                        warriors.add( new Warrior( "Character2", generator.generatorWarrior(), generator.generatorMana(), generator.generatorStrength()));
                     }
 
                     if (character2.equals("Wizard")){
-                        Wizard ch2 = new Wizard( "Character1", generator.generatorWizard(), generator.generatorMana(), generator.generatorIntelligence() );
+                        wizards.add(new Wizard( "Character1", generator.generatorWizard(), generator.generatorMana(), generator.generatorIntelligence() ));
                     } else {
-                        Warrior ch2 = new Warrior( "Character2", generator.generatorWarrior(), generator.generatorMana(), generator.generatorStrength());
+                        warriors.add(new Warrior( "Character2", generator.generatorWarrior(), generator.generatorMana(), generator.generatorStrength()));
                     }
+
                 case 5:
+                    warriors.clear();
+                    wizards.clear();
                     System.out.println("Closing program.");
                     scanner.close();
             }
@@ -62,6 +80,7 @@ public class Main {
     }
 
     private static void displayMenu() {
+        System.out.println("Welcome to Battlefield. \nBefore starting a battle please create two characters.");
         System.out.println("\nMenu:");
         System.out.println("1. Create Warrior");
         System.out.println("2. Create Wizard");
@@ -86,7 +105,7 @@ public class Main {
                 System.out.println("Sorry the number you enter is wrong. Please, enter a number between 100 and 200.");
             }
         }while(hp < 100 || hp > 200);
-
+        System.out.println(hp);
         do{
             System.out.println("Enter your warrior's stamina (10-50):");
             stamina = scanner.nextInt();
@@ -144,5 +163,126 @@ public class Main {
         Wizard wizard = new Wizard(name, hp, mana, intelligence);
         System.out.println("Congratulations! Your wizard "+wizard.getName()+" is created.");
         return wizard;
+    }
+
+    private static void battleOpposite(Warrior warrior, Wizard wizard){
+        String wizardName = wizard.getName();
+        String warriorName = warrior.getName();
+        int i = 1;
+        int wizardHp;
+        int warriorHp;
+        int damageWizard;
+        int damageWarrior;
+        String winner;
+
+        System.out.println(wizardName+" and "+warriorName+" get ready, the battle is going to start.");
+
+        do{
+            System.out.println("Round "+i);
+            wizardHp = wizard.getHp();
+            warriorHp = warrior.getHp();
+            wizard.Attack(warrior);
+            warrior.Attack(wizard);
+
+            damageWizard = wizardHp - wizard.getHp();
+            damageWarrior = warriorHp - warrior.getHp();
+
+            System.out.println(wizard+" attacks and receives "+damageWizard+" damage.");
+            System.out.println(warrior+" attacks and receives "+damageWarrior+" damage.");
+
+            i++;}while(wizard.checkIsAlive() && warrior.checkIsAlive());
+
+        if(!wizard.getIsAlive() && !wizard.getIsAlive()){
+            System.out.println("Oh no! Is a tie. Try a new battle and see who wins it all!");
+        }else{
+            if(wizard.getIsAlive()){
+                winner = wizardName;
+            }else{
+                winner = warriorName;
+            }
+            System.out.println("Congratulations, "+winner+" You won!");
+        }
+
+    }
+
+    private static void battleWarriors(Warrior warriorOne, Warrior warriorTwo){
+        String warriorOneName = warriorOne.getName();
+        String warriorTwoName = warriorTwo.getName();
+        int i = 1;
+        int warriorOneHp;
+        int warriorTwoHp;
+        int damageWarriorOne;
+        int damageWarriorTwo;
+        String winner;
+
+        System.out.println(warriorOneName+" and "+warriorTwoName+" get ready, the battle is going to start.");
+
+        do{
+            System.out.println("Round "+i);
+            warriorOneHp = warriorOne.getHp();
+            warriorTwoHp = warriorTwo.getHp();
+            warriorOne.Attack(warriorTwo);
+            warriorTwo.Attack(warriorOne);
+
+            damageWarriorOne = warriorOneHp - warriorOne.getHp();
+            damageWarriorTwo = warriorTwoHp - warriorTwo.getHp();
+
+            System.out.println(warriorOneName+" attacks and receives "+damageWarriorOne+" damage.");
+            System.out.println(warriorTwoName+" attacks and receives "+damageWarriorTwo+" damage.");
+            warriorOne.checkIsAlive();
+            warriorTwo.checkIsAlive();
+            i++;
+        }while(warriorOne.getIsAlive() && warriorTwo.getIsAlive());
+
+        if(!warriorOne.getIsAlive() && !warriorOne.getIsAlive()){
+            System.out.println("Oh no! Is a tie. Try a new battle and see who wins it all!");
+        }else{
+            if(warriorOne.getIsAlive()){
+                winner = warriorOneName;
+            }else{
+                winner = warriorTwoName;
+            }
+            System.out.println("Congratulations, "+winner+" You won!");
+        }
+
+    }
+
+    private static void battleWizards(Wizard wizardOne, Wizard wizardTwo){
+        String wizardOneName = wizardOne.getName();
+        String wizardTwoName = wizardTwo.getName();
+        int i = 1;
+        int wizardOneHp;
+        int wizardTwoHp;
+        int damageWizardOne;
+        int damageWizardTwo;
+        String winner;
+
+        System.out.println(wizardOneName+" and "+wizardTwoName+" get ready, the battle is going to start.");
+
+        do{
+            System.out.println("Round "+i);
+            wizardOneHp = wizardOne.getHp();
+            wizardTwoHp = wizardTwo.getHp();
+            wizardOne.Attack(wizardTwo);
+            wizardTwo.Attack(wizardOne);
+
+            damageWizardOne = wizardOneHp - wizardOne.getHp();
+            damageWizardTwo = wizardTwoHp - wizardTwo.getHp();
+
+            System.out.println(wizardOne+" attacks and receives "+damageWizardOne+" damage.");
+            System.out.println(wizardTwo+" attacks and receives "+damageWizardTwo+" damage.");
+
+            i++;}while(wizardOne.checkIsAlive() && wizardTwo.checkIsAlive());
+
+        if(!wizardOne.getIsAlive() && !wizardOne.getIsAlive()){
+            System.out.println("Oh no! Is a tie. Try a new battle and see who wins it all!");
+        }else{
+            if(wizardOne.getIsAlive()){
+                winner = wizardOneName;
+            }else{
+                winner = wizardTwoName;
+            }
+            System.out.println("Congratulations, "+winner+" You won!");
+        }
     }
 }
--- src/main/java/Warrior.java
+++ src/main/java/Warrior.java
@@ -15,25 +15,14 @@ public class Warrior extends Character implements Attacker{
     public int getStamina(){return stamina;}
     public int getStrength(){return strength;}
     public void setStamina(int stamina){
-        if(stamina < 10 || stamina > 50){
-            throw new IllegalArgumentException("Stamina isn't a number between 10 and 50.");
-        }else{
-        this.stamina = stamina;}
+        this.stamina = stamina;
     }
     public void setStrength(int strength) {
-        if(strength < 1 || strength > 10) {
-            throw new IllegalArgumentException("Strength isn't a number between 1 and 10.");
-        } else {
-            this.strength = strength;
-        }
+        this.strength = strength;
     }
     @Override
     public void setHp(int hp){
-      if(hp >200 || hp < 100){
-          throw new IllegalArgumentException("Warrior's hp isn't a number between 100 and 200.");
-      }else{
-          hp = hp;
-      }
+        super.setHp(hp);
     }
     public void Attack(Character x){
         int randomNumber = new Random().nextInt(2);
@@ -44,17 +33,17 @@ public class Warrior extends Character implements Attacker{
             if(staminaWarrior < 5){
                 weakAttack(x);
             }else{
-            heavyAttack(x);}
+                heavyAttack(x);}
         }else{
             //Weak attack
             if(staminaWarrior<1){
                 setStamina(staminaWarrior + 2);
             }else{
-            weakAttack(x);
+                weakAttack(x);
             }
         }
 
-        }
+    }
 
     public void heavyAttack(Character opponent){
         if(opponent == null){
